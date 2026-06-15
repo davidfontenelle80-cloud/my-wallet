e234f791892d284fb13442830738a85876281d99
davidfontenelle80-cloud <davidfontenelle80@gmail.com>
2026-06-15 06:44:34 -0400

    Add service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_VERSION = "my-wallet-v1";
+const PRECACHE = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icons/favicon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_VERSION).then((cache) => cache.addAll(PRECACHE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
